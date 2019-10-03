9b17764ea6151368856c39aa3028f590d3d3d56f
joaosilva <jv_rss@hotmail.com>
2019-10-03 14:18:40 -0300

    Mostrando como pegar os params

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({ message: "Hello World"});
+
+// GET, POST, PUT, DELETE
+// rec.query = Acessar query params (para filtros) 
+// rec.params = acessar route params (para edicao e delete)
+// rec.body = acessar corpo da requisicao (criacao, edicao)
+
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+    return res.json(req.body);
 })
 
 app.listen(3333);
