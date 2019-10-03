e6c8a6b754002f124535d7275016d99eae1a757f
joaosilva <jv_rss@hotmail.com>
2019-10-03 14:40:58 -0300

    Conexão com o mongo atlas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://omnistack:omnistack@oministack-dwiob.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // GET, POST, PUT, DELETE
+
 // rec.query = Acessar query params (para filtros) 
 // rec.params = acessar route params (para edicao e delete)
 // rec.body = acessar corpo da requisicao (criacao, edicao)
 
 app.use(express.json());
-
-app.post('/users', (req, res) => {
-    return res.json(req.body);
-})
-
+app.use(routes);
+ 
 app.listen(3333);
