fa367f3d6e47e160476954ddeafb4d8c6090a9d2
joaosilva <jv_rss@hotmail.com>
2019-10-07 10:29:06 -0300

    Adicionando CORS ao servidor node

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const app = express();
@@ -15,6 +17,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@oministack-dwiob.mongodb.net
 // rec.params = acessar route params (para edicao e delete)
 // rec.body = acessar corpo da requisicao (criacao, edicao)
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
  
