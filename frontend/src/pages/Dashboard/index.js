a289a5254f10ae54ac69c8766d290764a17a0f81
joaosilva <jv_rss@hotmail.com>
2019-10-07 13:28:46 -0300

    remoção de linha sem uso

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../services/api'
-import { list } from 'postcss';
 
 import './styles.css';
 
